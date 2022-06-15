659d2492235caf85e82041152d719622d082d239
ThomasByr <tbouyer2000@gmail.com>
2022-06-15 16:15:15 +0200

    cartpole simulation v0.1.0

---
+++ core/tests/train_test.py
@@ -0,0 +1,20 @@
+from termcolor import colored
+
+from ..main import *
+
+
+def test_train():
+    num_actions = env.action_space.n  # 2
+
+    num_hidden_units = 128
+
+    model = ActorCritic(num_actions, num_hidden_units)
+    assert model is not None
+
+    try:
+        train(model)
+    except Exception as e:
+        print(colored(f"Error: {e}", "red"))
+        assert False
+
+    assert True
