fe60d4116e231c0e9cc97062995d7d94c99d44e0
Thomas Byr <tbouyer2000@gmail.com>
2022-06-16 18:38:00 +0200

    cartpole simulation v1.1.2 [tests]

--- src/tests/train_test.py
+++ src/tests/train_test.py
@@ -1,17 +1,41 @@
 import pytest
+import tensorflow as tf
+import numpy as np
 
-from ..core import Env
+from ..core import Env, ActorCritic
 
 
 @pytest.mark.filterwarnings("ignore:.*:DeprecationWarning")
 def test_v0():
     """Test the train function."""
     environment = Env("0")
-    environment.run()
+
+    seed = 42
+    environment.env.reset(seed=seed)
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
+
+    num_actions = environment.env.action_space.n
+
+    num_hidden_units = 1 << 7
+
+    model = ActorCritic(num_actions, num_hidden_units)
+    environment.train(model)
 
 
 @pytest.mark.filterwarnings("ignore:.*:DeprecationWarning")
 def test_v1():
     """Test the train function."""
     environment = Env("1")
-    environment.run()
+
+    seed = 42
+    environment.env.reset(seed=seed)
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
+
+    num_actions = environment.env.action_space.n
+
+    num_hidden_units = 1 << 7
+
+    model = ActorCritic(num_actions, num_hidden_units)
+    environment.train(model)
