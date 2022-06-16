f0da5f03df725639a0d8283d1adab1c663c7b675
ThomasByr <tbouyer2000@gmail.com>
2022-06-16 15:26:15 +0200

    cartpole simulation v1.1.1 [tests]

--- src/tests/train_test.py
+++ src/tests/train_test.py
@@ -1,22 +1,17 @@
 import pytest
-from termcolor import colored
 
-from ..core import ActorCritic, train, env
+from ..core import Env
 
 
 @pytest.mark.filterwarnings("ignore:.*:DeprecationWarning")
-def test_train():
-    num_actions = env.action_space.n  # 2
+def test_v0():
+    """Test the train function."""
+    environment = Env("0")
+    environment.run()
 
-    num_hidden_units = 128
 
-    model = ActorCritic(num_actions, num_hidden_units)
-    assert model is not None
-
-    try:
-        train(model)
-    except Exception as e:
-        print(colored(f"Error: {e}", "red"))
-        assert False
-
-    assert True
+@pytest.mark.filterwarnings("ignore:.*:DeprecationWarning")
+def test_v1():
+    """Test the train function."""
+    environment = Env("1")
+    environment.run()
