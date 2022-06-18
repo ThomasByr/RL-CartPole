2b91917ccada7e35fa7828b5c93abb6b90940eeb
Thomas Byr <tbouyer2000@gmail.com>
2022-06-18 23:20:37 +0200

    cartpole simulation v1.1.5 [cfg]

---
+++ src/cfg/device.py
@@ -0,0 +1,36 @@
+"""
+Device
+
+Device enum to force use with a specific device.
+"""
+
+#pylint: disable=[C0103, C0410, C0411]
+
+from enum import Enum
+
+import tensorflow as tf
+
+__all__ = ["Device"]
+
+n = len(tf.config.experimental.list_physical_devices("GPU"))
+
+
+class Device(Enum):
+    """Enum for the different devices."""
+
+    cpu = "cpu"  # CPU
+    gpu = "gpu"  # GPU
+    auto = "auto"  # Auto
+
+    def get_name(self) -> str:
+        """Get the name of the device."""
+        return {
+            self.cpu: "/cpu:0",
+            self.gpu: "/gpu:0",
+            self.auto: "/gpu:0" if n > 0 else "/cpu:0",
+        }[self]
+
+    @staticmethod
+    def n() -> int:
+        """Get the number of available devices."""
+        return n
