6965d5a1236bed6c271a3f86376c487def9990c0
Thomas Byr <tbouyer2000@gmail.com>
2022-06-19 22:26:37 +0200

    cartpole simulation v1.1.6 [src]

--- src/__version__.py
+++ src/__version__.py
@@ -5,7 +5,7 @@ Version information for the package.
 __title__ = "cartpole"
 __description__ = "A CartPole implementation in Python using reinforcement learning."
 __url__ = "https://github.com/ThomasByr/RL-CartPole"
-__version__ = "1.1.5"
+__version__ = "1.1.6"
 __author__ = "Thomas Byr"
 __author_email__ = "tbouyer2000@gmail.com"
 __license__ = "GNU GPLv3"
