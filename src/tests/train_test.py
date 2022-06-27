7be4c4aa28635825acdc3fe455261ad6f27dbefb
ThomasByr <tbouyer2000@gmail.com>
2022-06-27 08:46:34 +0200

    cartpole simulation v1.1.6 [tests]

--- src/tests/train_test.py
+++ src/tests/train_test.py
@@ -12,39 +12,39 @@ from ..cfg import Device
 
 @pytest.mark.filterwarnings("ignore:.*:DeprecationWarning")
 def test_v0():
-    """Test the train function."""
-    environment = Env("0")
+  """Test the train function."""
+  environment = Env("0")
 
-    with tf.device(Device.cpu.get_name()):
-        seed = 42
-        environment.env.reset(seed=seed)
-        tf.random.set_seed(seed)
-        np.random.seed(seed)
+  with tf.device(Device.cpu.get_name()):
+    seed = 42
+    environment.env.reset(seed=seed)
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
 
-        num_actions = environment.env.action_space.n
+    num_actions = environment.env.action_space.n
 
-        num_hidden_units = 1 << 7
+    num_hidden_units = 1 << 7
 
-        model = ActorCritic(num_actions, num_hidden_units)
+    model = ActorCritic(num_actions, num_hidden_units)
 
-    environment.train(model)
+  environment.train(model)
 
 
 @pytest.mark.filterwarnings("ignore:.*:DeprecationWarning")
 def test_v1():
-    """Test the train function."""
-    environment = Env("1")
+  """Test the train function."""
+  environment = Env("1")
 
-    with tf.device(Device.cpu.get_name()):
-        seed = 42
-        environment.env.reset(seed=seed)
-        tf.random.set_seed(seed)
-        np.random.seed(seed)
+  with tf.device(Device.cpu.get_name()):
+    seed = 42
+    environment.env.reset(seed=seed)
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
 
-        num_actions = environment.env.action_space.n
+    num_actions = environment.env.action_space.n
 
-        num_hidden_units = 1 << 7
+    num_hidden_units = 1 << 7
 
-        model = ActorCritic(num_actions, num_hidden_units)
+    model = ActorCritic(num_actions, num_hidden_units)
 
-    environment.train(model)
+  environment.train(model)
