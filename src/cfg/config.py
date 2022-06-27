45b703dbcfce20eb3eeebec4759744d38b882fc4
ThomasByr <tbouyer2000@gmail.com>
2022-06-27 08:55:51 +0200

    cartpole simulation v1.1.6 [cfg]

--- src/cfg/config.py
+++ src/cfg/config.py
@@ -44,27 +44,32 @@ class Config(Enum):
     }[self]
 
 
+def _format_msg(msg: str, *args: object) -> str:
+  """Format a message."""
+  return msg % args if re.search(r"%[sdf]|%.[0-9]f", msg) else msg.format(*args)
+
+
 def info(msg: str, *args: object) -> None:
   """Print a message to stdout to the info level."""
-  print(colored("   INFO", "green"), msg.format(*args))
+  print(colored("   INFO", "green"), _format_msg(msg, *args))
 
 
 def debug(msg: str, *args: object) -> None:
   """Print a message to stdout to the debug level."""
-  print(colored("  DEBUG", "cyan"), msg.format(*args))
+  print(colored("  DEBUG", "cyan"), _format_msg(msg, *args))
 
 
 def warn(msg: str, *args: object) -> None:
   """Print a message to stdout to the warn level."""
-  print(colored("   WARN", "yellow"), msg.format(*args))
+  print(colored("   WARN", "yellow"), _format_msg(msg, *args))
 
 
 def error(msg: str, *args: object) -> None:
   """Print a message to stdout to the error level."""
-  print(colored("  ERROR", "red"), msg.format(*args))
+  print(colored("  ERROR", "red"), _format_msg(msg, *args))
 
 
 def fatal(msg: str, *args: object) -> NoReturn:
   """Print a message to stdout to the fatal level and exit."""
-  print(colored("  FATAL", "red"), msg.format(*args))
+  print(colored("  FATAL", "red"), _format_msg(msg, *args))
   sys.exit(1)
